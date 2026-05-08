9db74a66061bd4684c7cce0e1d13d8443bb39357
alecproj <alecproj@yandex.ru>
2026-05-08 11:17:48 +0300

    Implement sender script

---
+++ sender/run.py
@@ -0,0 +1,50 @@
+import time
+import argparse
+
+import meshtastic.serial_interface
+import meshtastic.tcp_interface
+
+
+def connect(args):
+    if args.host:
+        return meshtastic.tcp_interface.TCPInterface(hostname=args.host, noProto=False)
+
+    if args.port:
+        return meshtastic.serial_interface.SerialInterface(devPath=args.port)
+
+    return meshtastic.serial_interface.SerialInterface()
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--port", help="Например /dev/ttyUSB0")
+parser.add_argument("--host", help="IP Meshtastic-устройства по Wi-Fi")
+parser.add_argument("--interval", type=float, default=60.0)
+parser.add_argument("--run-id", default="test01")
+
+args = parser.parse_args()
+
+interface = connect(args)
+
+seq = 0
+
+try:
+    while True:
+        tx_time_ms = int(time.time() * 1000)
+
+        text = (
+            f"RT,"
+            f"run={args.run_id},"
+            f"seq={seq},"
+            f"tx_ms={tx_time_ms}"
+        )
+
+        interface.sendText(text, wantAck=False)
+
+        print(text)
+
+        seq += 1
+        time.sleep(args.interval)
+
+except KeyboardInterrupt:
+    interface.close()
